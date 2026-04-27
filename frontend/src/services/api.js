097dae86de5a1ed927fb8807942478ac4b3c364b
SuhaniMogra <suhanimogra2004@gmail.com>
2026-04-27 20:43:12 +0530

    Restore frontend API exports with Railway support

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -9,4 +9,35 @@ const api = axios.create({
   baseURL: API_BASE,
 });
 
+// Research Agent
+export const researchQuery = async (query) => {
+  const response = await api.post("/research", null, {
+    params: { query },
+  });
+  return response.data;
+};
+
+// Document Upload
+export const uploadDocument = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await api.post("/upload-doc", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
+
+// Document Q&A
+export const askDocument = async (question) => {
+  const response = await api.post("/ask-doc", null, {
+    params: { question },
+  });
+
+  return response.data;
+};
+
 export default api;
